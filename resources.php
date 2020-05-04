75929eaf95ddfb8c4a5b4c336a7f6f1ffcc03b4e
leandroprada <leandroprada@gmail.com>
2020-05-03 21:36:08 -0300

    attempt for PHP reconfiguration Initial

---
+++ resources.php
@@ -0,0 +1,14 @@
+<?php
+ session_start();
+$_SESSION['username'] = $_POST['username'];
+$_SESSION['email'] = $_POST['email'];
+
+
+if ($_SESSION['login'] == "validated") {
+	include "./content/resources/content_logged_in.php";
+	}
+else {
+	include "./content/resources/content_not_logged_in.php";
+	}
+
+?>
