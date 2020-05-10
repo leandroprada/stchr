6235646e3edcad6d103fe80032141e8d21388f22
leandroprada <leandroprada@gmail.com>
2020-05-10 14:53:04 -0300

    update nombre en resources

--- resources.php
+++ resources.php
@@ -3,6 +3,20 @@
 $_SESSION['username'] = $_POST['username'];
 $_SESSION['email'] = $_POST['email'];
 
+if (isset($_SESSION['userloggedname'])) {
+	
+	$firstname = $_SESSION['userloggedname']
+}
+
+elseif (isset($_SESSION['userloggedmail'])) {
+	
+	$firstname = $_SESSION['userloggedmail']
+}
+
+else {
+	
+	$firstname = "Usuario";
+}
 
 if ($_SESSION['login'] == "validated") {
 	include "./content/resources/content_logged_in.php";
