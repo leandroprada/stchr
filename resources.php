1314eb2262036c802f6ef1092d3f51610332da47
leandroprada <leandroprada@gmail.com>
2020-05-10 15:06:39 -0300

    update nombre en resources

--- resources.php
+++ resources.php
@@ -7,11 +7,15 @@ echo "this is in SESSION";
 var_dump ($_SESSION);
 
 if ($_SESSION['userloggedname'] != NULL) {
+	echo "ESTOY SETEANDO FIRST NAME (Viene de userloggedname A";
 	$firstname = $_SESSION['userloggedname'];
+	echo $firstname;
 }
 
 if ($_SESSION['userloggedemail'] != NULL) {
+	echo "ESTOY SETEANDO FIRST NAME (Viene de userloggedemail A";
 	$firstname = $_SESSION['userloggedemail'];
+	echo $firstname;
 }
 
 var_dump ($firstname);
