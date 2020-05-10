9e666c37079b7e4314611624663c3c71f36e2d73
leandroprada <leandroprada@gmail.com>
2020-05-10 14:14:46 -0300

    changed novedades

--- content/resources/content_logged_in.php
+++ content/resources/content_logged_in.php
@@ -149,6 +149,20 @@
         </div>
       </div>
     </section>
+	<section class="video-promo section">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 col-sm-12">
+              <div class="video-promo-content text-center">
+               <iframe width="560" height="315" src="https://www.youtube.com/embed/c1YSfFaMve4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Presentaci&oacute;n</h2>
+                <p class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Son s&oacute;lo dos minutos</p>
+              </div>
+          </div>
+        </div>
+      </div>
+    </section>
     <!-- End Video Promo Section -->
 
     
