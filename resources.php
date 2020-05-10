ca9d2c30ba1e73f77be34e0e4a5c9602cbecef96
leandroprada <leandroprada@gmail.com>
2020-05-10 15:11:27 -0300

    update nombre en resources

--- resources.php
+++ resources.php
@@ -3,22 +3,14 @@
 $_SESSION['username'] = $_POST['username'];
 $_SESSION['email'] = $_POST['email'];
 
-echo "this is in SESSION";
-var_dump ($_SESSION);
-
 if ($_SESSION['userloggedname'] != NULL) {
-	echo "ESTOY SETEANDO FIRST NAME (Viene de userloggedname A";
 	$firstname = $_SESSION['userloggedname'];
-	echo $firstname;
+	
 }
 
 if ($_SESSION['userloggedemail'] != NULL) {
-	echo "ESTOY SETEANDO FIRST NAME (Viene de userloggedemail A";
 	$firstname = $_SESSION['userloggedemail'];
-	echo $firstname;
 }
-
-var_dump ($firstname);
 
 if ($_SESSION['login'] == "validated") {
 	include "./content/resources/content_logged_in.php";
