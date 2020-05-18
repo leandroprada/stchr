f34ff703a55197eaac7f9bc84cd20bbb7ceb0a3d
leandroprada <leandroprada@gmail.com>
2020-05-17 22:00:46 -0300

    updated resources with video from IT training

--- content/resources/content_logged_in.php
+++ content/resources/content_logged_in.php
@@ -141,7 +141,7 @@ $firstname = $_SESSION['firstname'];
 
 		  <div class="col-md-12 col-sm-12">
 		    
-              <div class="video-promo-content-light text-center">
+              <div class="video-promo-content text-center">
 					<h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT Avanzado 16-MAY-2020</h2>
 					<iframe width="560" height="315" src="https://www.youtube.com/embed/JRn1b-pxX40" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
               </div>
