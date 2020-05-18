4146f6379cceecf16874955938ebb8674844c4b1
leandroprada <leandroprada@gmail.com>
2020-05-18 08:26:19 -0300

    updated resources with video from IT training

--- content/resources/content_logged_in.php
+++ content/resources/content_logged_in.php
@@ -127,6 +127,7 @@ $firstname = $_SESSION['firstname'];
 	
     <!-- Start Video promo Section -->
 
+	
 	<section class="video-promo section">
       <div class="overlay"></div>
       <div class="container">
@@ -135,7 +136,7 @@ $firstname = $_SESSION['firstname'];
 		    
               <div class="video-promo-content text-center">
 					<h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT Avanzado 09-MAY-2020</h2>
-					<iframe width="560" height="315" src="https://www.youtube.com/embed/c1YSfFaMve4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					<iframe width="75%" height="315" src="https://www.youtube.com/embed/c1YSfFaMve4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
               </div>
           </div>
 
@@ -143,7 +144,7 @@ $firstname = $_SESSION['firstname'];
 		    
               <div class="video-promo-content text-center">
 					<h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT Avanzado 16-MAY-2020</h2>
-					<iframe width="560" height="315" src="https://www.youtube.com/embed/JRn1b-pxX40" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					<iframe width="75%" height="315" src="https://www.youtube.com/embed/JRn1b-pxX40" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
               </div>
           </div>
         </div>
