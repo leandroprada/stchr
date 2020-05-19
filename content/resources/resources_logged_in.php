25b527760d58992492ada80e65fd138c4b7b8195
leandroprada <leandroprada@gmail.com>
2020-05-19 11:53:00 -0300

    framing the dynamic content for resources

--- content/resources/resources_logged_in.php
+++ content/resources/resources_logged_in.php
@@ -122,38 +122,18 @@ $firstname = $_SESSION['firstname'];
     </section>
     <!-- Services Section End -->
 
-    
+<?php
+ 
+			 if ($_SESSION['login'] == "validated") {
+				include './content/resources_by_curso.php';
+				}
+			else {
+				include './content/resources_generico.php';
+				}
+			  
+			 ?>    
 		
 	
-    <!-- Start Video promo Section -->
-
-	
-	<section class="video-promo section">
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12 col-sm-12">
-		    
-              <div class="video-promo-content text-center">
-					<h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT Avanzado 09-MAY-2020</h2>
-					<iframe width="75%" height="315" src="https://www.youtube.com/embed/c1YSfFaMve4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-              </div>
-          </div>
-
-		  <div class="col-md-12 col-sm-12">
-		    
-              <div class="video-promo-content text-center">
-					<h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT Avanzado 16-MAY-2020</h2>
-					<iframe width="75%" height="315" src="https://www.youtube.com/embed/JRn1b-pxX40" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-              </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <!-- End Video Promo Section -->
-
-    
-
     <!-- Team section Start -->
     <section id="team" class="section">
       <div class="container">
