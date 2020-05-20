f5ea368c52c43c9ba0272d007880434777d09c86
leandroprada <leandroprada@gmail.com>
2020-05-20 16:33:59 -0300

    pushing videos to be local

--- content/resources/resources_by_curso.php
+++ content/resources/resources_by_curso.php
@@ -26,8 +26,8 @@ $course = $_SESSION['course'];
 							  <div class="video-promo-content text-center">
 									<h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT Avanzado 09-MAY-2020</h2>
 								 <iframe width="75%" height="315" src="https://www.youtube.com/embed/c1YSfFaMve4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-									<iframe width="75%" height="450px" src="press1ForSMS.mp4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-									<object data="press1ForSMS.mp4" width="75%" height="450px"></object>
+									<iframe width="75%" height="450px" src="" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen><object data="press1ForSMS.mp4" width="75%" height="450px"></object></iframe>
+									
 							  </div>
 						  </div>
 
