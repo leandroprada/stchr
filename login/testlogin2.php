a8f85b3f928b1f542fce425310b9d111bc169d03
leandroprada <leandroprada@gmail.com>
2020-05-22 09:16:36 -0300

    testing the group access as a list

--- login/testlogin2.php
+++ login/testlogin2.php
@@ -34,7 +34,7 @@ if(strpos($useroremail, $arroba) !== false){
 	
 	$queryn = $queryn1.$queryn2.$queryn3.$queryn4;
 	echo $queryn;
-	/*$resultn = mysqli_query($conn,$queryn);
+	$resultn = mysqli_query($conn,$queryn);
 	$rown = mysqli_fetch_row($resultn);
 	$name = $rown[0];
  
