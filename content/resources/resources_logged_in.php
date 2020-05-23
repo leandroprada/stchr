a6e49ca6f438a495f458090c295e56e2383ace57
leandroprada <leandroprada@gmail.com>
2020-05-23 13:22:55 -0300

    code refactoring

--- content/resources/resources_logged_in.php
+++ content/resources/resources_logged_in.php
@@ -158,7 +158,7 @@ $courses = $_SESSION['courses'];
 		// En el contenido de esta seccion es dinamico de acuerdo al grupo al que pertenece cada usuario
 	
 		if ($coursename != NULL) {
-				if ($coursename == "default"){
+				if ($coursename == "generico"){
 				echo '
 					<!-- Start Video promo Section -->
 						<section class="video-promo section">
