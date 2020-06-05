bb1034e306b9aa605aced01156f6f9ee7b038727
leandroprada <leandroprada@gmail.com>
2020-06-05 08:49:25 -0300

    updating video scheme

--- content/resources/resources_itbasicojun20.php
+++ content/resources/resources_itbasicojun20.php
@@ -12,13 +12,19 @@
 			<div class="row">
 			  <div class="col-md-12 col-sm-12">
 				  <div class="video-promo-content text-center">
-						<h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 1 JUN-2020</h2>
-					 <iframe width="75%" height="315" src="/content/resources/videos/itbasicojun20.mp4" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						<ul>
+							<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 1 JUN-2020 <a  href="#telecomSubmenu2" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
+								<div class="collapse list-unstyled" id="telecomSubmenu2">
+									
+									<video width="100%" height="450px"  controls>
+									  <source src="content/resources/videos/itbasicojun20.mp4" type="video/mp4">
+									Your browser does not support the video tag.
+									</video>
+								</div>
+							</li>
+						</ul>	
 				  </div>
 			  </div>
-
-			
-			  
 			</div>
 		  </div>
 		</section>
