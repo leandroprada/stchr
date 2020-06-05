84c15510fbaa0d8d7dc4b516eef9de479fcf49a0
leandroprada <leandroprada@gmail.com>
2020-06-05 09:18:05 -0300

    updating video scheme

--- content/resources/resources_itavanzadomay20.php
+++ content/resources/resources_itavanzadomay20.php
@@ -4,7 +4,7 @@
  
  <!-- Start Video promo Section -->
 	<section class="video-promo section" style="background:url(../img/bg/video1.jpg);">
-		<div class="overlay"></div>
+	
 	  <div class="container" >
 		<div class="row">
 		  <div class="col-md-12 col-sm-12">
