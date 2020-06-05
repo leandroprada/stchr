40d02c15b3166d121dc46fec0f41e49714934715
leandroprada <leandroprada@gmail.com>
2020-06-05 08:31:52 -0300

    updating video scheme

--- content/resources/resources_itavanzadomay20.php
+++ content/resources/resources_itavanzadomay20.php
@@ -3,43 +3,40 @@
  ?>
  
  <!-- Start Video promo Section -->
-		<section class="video-promo section">
-		  <div class="overlay"></div>
-		  <div class="container">
-			<div class="row">
-			  <div class="col-md-12 col-sm-12">
-				  <div class="video-promo-content text-center">
-						<h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT Avanzado 09-MAY-2020</h2>
-						
-						<li style="text-decoration:none;padding-bottom:10px; "><a  href="#telecomSubmenu2" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-sm"  style="color:white;background-color:#825EAB;">Ver </a>
-							Avaya CPaaS API webinar
-						<div class="collapse list-unstyled" id="telecomSubmenu2">
-							<h3 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Avaya CPaaS API webinar</h3>
-							<video width="75%" height="450px"  controls>
-							  <source src="../content/resources/videos/itavanzadomay20-1.mp4" type="video/mp4">
-							Your browser does not support the video tag.
-							</video>
-						</div>
-					</li>
-					 <iframe width="75%" height="315" src="/content/resources/videos/itavanzadomay20-1.mp4" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-				  </div>
+	<section class="video-promo section">
+		<div class="overlay"></div>
+	  <div class="container">
+		<div class="row">
+		  <div class="col-md-12 col-sm-12">
+			  <div class="video-promo-content text-center">
+				<li style="text-decoration:none;padding-bottom:10px; "><a  href="#telecomSubmenu2" data-toggle="collapse" aria-expanded="false" class="btn btn-success btn-lg"  style="color:white;">Ver </a><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT Avanzado 09-MAY-2020</h2>							
+					<div class="collapse list-unstyled" id="telecomSubmenu2">
+						<h3 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Avaya CPaaS API webinar</h3>
+						<video width="75%" height="450px"  controls>
+						  <source src="content/resources/videos/itavanzadomay20-1.mp4" type="video/mp4">
+						Your browser does not support the video tag.
+						</video>
+					</div>
+				</li>
+				<li style="text-decoration:none;padding-bottom:10px; "><a  href="#telecomSubmenu2" data-toggle="collapse" aria-expanded="false" class="btn btn-success btn-lg"  style="color:white;">Ver </a><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT Avanzado Clase 2 16-MAY-2020</h2>							
+					<div class="collapse list-unstyled" id="telecomSubmenu2">
+						<video width="75%" height="450px"  controls>
+						  <source src="content/resources/videos/itavanzadomay20-2.mp4" type="video/mp4">
+						Your browser does not support the video tag.
+						</video>
+					</div>
+				</li>
+				<li style="text-decoration:none;padding-bottom:10px; "><a  href="#telecomSubmenu2" data-toggle="collapse" aria-expanded="false" class="btn btn-success btn-lg"  style="color:white;">Ver </a><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT Avanzado &Uacute;ltima Clase 24-MAY-2020</h2>							
+					<div class="collapse list-unstyled" id="telecomSubmenu2">
+						<video width="75%" height="450px"  controls>
+						  <source src="content/resources/videos/itavanzadomay20-3.mp4" type="video/mp4">
+						Your browser does not support the video tag.
+						</video>
+					</div>
+				</li>
 			  </div>
-
-			  <div class="col-md-12 col-sm-12">
-				  <div class="video-promo-content text-center">
-						<h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT Avanzado Clase 2 16-MAY-2020</h2>
-						<iframe width="75%" height="315" src="/content/resources/videos/itavanzadomay20-2.mp4" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-				  </div>
-			  </div>
-			  
-			    <div class="col-md-12 col-sm-12">
-				  <div class="video-promo-content text-center">
-						<h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT Avanzado &Uacute;ltima Clase 24-MAY-2020</h2>
-						<iframe width="75%" height="315" src="/content/resources/videos/itavanzadomay20-3.mp4" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-				  </div>
-			  </div>
-			  
-			</div>
 		  </div>
-		</section>
+		</div>
+	  </div>
+	</section>
 <!-- End Video Promo Section -->
