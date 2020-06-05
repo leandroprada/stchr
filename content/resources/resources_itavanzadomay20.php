90742fe38992ac666d8f823a4d647fd81b8a7b61
leandroprada <leandroprada@gmail.com>
2020-06-05 09:24:12 -0300

    updating video scheme

--- content/resources/resources_itavanzadomay20.php
+++ content/resources/resources_itavanzadomay20.php
@@ -3,15 +3,15 @@
  ?>
  
  <!-- Start Video promo Section -->
-	<section class="video-promo section" style="background:url(../img/bg/video1.jpg);">
+	<section class="video-promo section " >
 	
 	  <div class="container" >
 		<div class="row">
 		  <div class="col-md-12 col-sm-12">
 			  <div class="video-promo-content text-center">
 				<ul>
-					<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT Avanzado 09-MAY-2020 <a  href="#telecomSubmenu2" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
-						<div class="collapse list-unstyled" id="telecomSubmenu2">
+					<li style="text-decoration:none;padding-bottom:10px; "><h3 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT Avanzado 09-MAY-2020 <a  href="#submenuavmay1" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h3>							
+						<div class="collapse list-unstyled" id="submenuavmay1">
 							
 							<video width="100%" height="450px"  controls>
 							  <source src="content/resources/videos/itavanzadomay20-1.mp4" type="video/mp4">
@@ -19,16 +19,16 @@
 							</video>
 						</div>
 					</li>
-					<li style="text-decoration:none;padding-bottom:10px; "> <h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT Avanzado Clase 2 16-MAY-2020 <a  href="#telecomSubmenu2" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver </a></h2>							
-						<div class="collapse list-unstyled" id="telecomSubmenu2">
+					<li style="text-decoration:none;padding-bottom:10px; "> <h3 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT Avanzado Clase 2 16-MAY-2020 <a  href="#submenuavmay2" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver </a></h3>							
+						<div class="collapse list-unstyled" id="submenuavmay2">
 							<video width="75%" height="450px"  controls>
 							  <source src="content/resources/videos/itavanzadomay20-2.mp4" type="video/mp4">
 							Your browser does not support the video tag.
 							</video>
 						</div>
 					</li>
-					<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT Avanzado &Uacute;ltima Clase 24-MAY-2020 <a  href="#telecomSubmenu2" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver </a> </h2>
-						<div class="collapse list-unstyled" id="telecomSubmenu2">
+					<li style="text-decoration:none;padding-bottom:10px; "><h3 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT Avanzado &Uacute;ltima Clase 24-MAY-2020 <a  href="#submenuavmay3" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver </a> </h3>
+						<div class="collapse list-unstyled" id="submenuavmay3">
 													
 							<video width="75%" height="450px"  controls>
 							  <source src="content/resources/videos/itavanzadomay20-3.mp4" type="video/mp4">
