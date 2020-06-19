e48886d54f4efc40e26918591f45fe3925e852df
leandroprada <leandroprada@gmail.com>
2020-06-19 18:53:30 -0300

    added course

--- content/resources/resources_itbasicojun20.php
+++ content/resources/resources_itbasicojun20.php
@@ -26,8 +26,8 @@
 				  </div>
 				  <div class="video-promo-content text-center">
 						<ul>
-							<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 19 JUN-2020 <a  href="#submenubasico1" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
-								<div class="collapse list-unstyled" id="submenubasico1">
+							<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 19 JUN-2020 <a  href="#submenubasico2" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
+								<div class="collapse list-unstyled" id="submenubasico2">
 									
 									<video width="100%" height="450px"  controls>
 									  <source src="content/resources/videos/itbasicojun20-2.mp4" type="video/mp4">
