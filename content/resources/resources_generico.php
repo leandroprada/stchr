146d7b2faafbaa2477d851914d6663f1d28419bc
leandroprada <leandroprada@gmail.com>
2020-07-23 18:41:50 -0300

    added interview to Romina in Generico

--- content/resources/resources_generico.php
+++ content/resources/resources_generico.php
@@ -9,8 +9,8 @@
         <div class="row">
           <div class="col-md-12 col-sm-12">
               <div class="video-promo-content text-center">
-				<h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Contenido exclusivo</h2>
-                <iframe width="75%"	 height="450px" src="https://www.youtube.com/embed/uQ8W_Zpm1Zg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
+				<h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Entrevista de DuHire a nuestra directora, Romina C&oacute;rdoba</h2>
+                <iframe width="75%"	 height="450px" src="https://www.youtube.com/embed/TCOEEOvKXXk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
                 
               </div>
           </div>
