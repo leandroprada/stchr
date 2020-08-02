9aaa5e06c42fb933d1194d515501a881db7a617e
leandroprada <leandroprada@gmail.com>
2020-08-02 09:02:26 -0300

    added third episode of July training

--- content/resources/resources_itbasicojul20.php
+++ content/resources/resources_itbasicojul20.php
@@ -27,7 +27,7 @@
 				  
 				  <div class="video-promo-content text-center">
 						<ul>
-							<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 18 JUL-2020 <a  href="#submenubasicojul2" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
+							<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 25 JUL-2020 <a  href="#submenubasicojul2" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
 								<div class="collapse list-unstyled" id="submenubasicojul2">
 									
 									<video width="100%" height="450px"  controls>
