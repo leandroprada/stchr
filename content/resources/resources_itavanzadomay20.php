b318232fcaba979b39292cd8652ef249fc53a5f9
leandro prada <leandroprada@gmail.com>
2020-12-06 12:54:36 -0300

    deleted youtube for old course

--- content/resources/resources_itavanzadomay20.php
+++ content/resources/resources_itavanzadomay20.php
@@ -13,9 +13,9 @@
 					<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 2 Clase 1  09-MAY-2020 <a  href="#submenuavmay1" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
 						<div class="collapse list-unstyled" id="submenuavmay1">
 							
-							
-							  
-							  <iframe width="560" height="315" src="https://www.youtube.com/embed/kUHHFt8MIUM?controls=0" frameborder="0" allow="accelerometer; ; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true" allownetworking="internal" controls=0 modestbranding=1></iframe> 
+							<video width="100%" height="450px"  controls>
+							  <source src="content/resources/videos/itavanzadomay20-1.mp4" type="video/mp4">
+							Your browser does not support the video tag.
 							</video>
 						</div>
 					</li>
