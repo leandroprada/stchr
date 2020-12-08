c9e714e4318f08ace92b56254b09d16523513872
leandro prada <leandroprada@gmail.com>
2020-12-08 11:37:31 -0300

    added rrss course

--- content/resources/resources_rrssemprendedoresdic20.php
+++ content/resources/resources_rrssemprendedoresdic20.php
@@ -20,6 +20,15 @@
 						</div>
 					</li>
 					
+					<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Potenci&aacute; tus redes sociales - Emprendedores<a  href="#submenurrssdic202" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
+						<div class="collapse list-unstyled" id="submenurrssdic202">
+							
+							<video width="100%" height="450px"  controls>
+							  <source src="content/resources/videos/rrssParaEmprendedores-2.mp4" type="video/mp4">
+							Your browser does not support the video tag.
+							</video>
+						</div>
+					</li>
 				</ul>
 			  </div>
 		  </div>
