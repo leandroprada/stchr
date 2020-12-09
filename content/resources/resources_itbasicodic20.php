fc6aa0c80fa6290f96b471aa2df1ffee629c6546
leandro prada <leandroprada@gmail.com>
2020-12-09 13:37:38 -0300

    added curso basico diciembre

---
+++ content/resources/resources_itbasicodic20.php
@@ -0,0 +1,54 @@
+<?php
+ session_start(); 
+ ?>
+ 
+ <!-- Start Video promo Section -->
+	<section class="video-promo section " >
+	<div class="overlay-course"></div>
+	  <div class="container" >
+		<div class="row">
+		  <div class="col-md-12 col-sm-12">
+			  <div class="video-promo-content text-center">
+				<ul>
+					<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 1 Clase 1 07-Dic-2020 <a  href="#submenubasicodic201" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
+						<div class="collapse list-unstyled" id="submenubasicodic201">
+							
+							<video width="100%" height="450px"  controls>
+							  <source src="content/resources/videos/itbasicodic20-1.mp4" type="video/mp4">
+							Your browser does not support the video tag.
+							</video>
+						</div>
+					</li>
+					<li style="text-decoration:none;padding-bottom:10px; "> <h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 1 Clase 2 08-Dic-2020 <a  href="#submenubasicodic202" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver </a></h2>							
+						<div class="collapse list-unstyled" id="submenubasicodic202">
+							<video width="75%" height="450px"  controls>
+							  <source src="content/resources/videos/itbasicodic20-2.mp4" type="video/mp4">
+							Your browser does not support the video tag.
+							</video>
+						</div>
+					</li>
+					<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 1 Clase 3 09-Dic-2020 <a  href="#submenubasicodic203" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver </a> </h2>
+						<div class="collapse list-unstyled" id="submenubasicodic203">
+													
+							<video width="75%" height="450px"  controls>
+							  <source src="content/resources/videos/itbasicodic20-3.mp4" type="video/mp4">
+							Your browser does not support the video tag.
+							</video>
+						</div>
+					</li>
+					<!-- <li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 2 Bonus Clase 4 12-SEP-2020 <a  href="#submenubasicodic204" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver </a> </h2>
+						<div class="collapse list-unstyled" id="submenubasicodic204">
+													
+							<video width="75%" height="450px"  controls>
+							  <source src="content/resources/videos/itavanzadonov20-4.mp4" type="video/mp4">
+							Your browser does not support the video tag.
+							</video>
+						</div>
+					</li> -->
+				</ul>
+			  </div>
+		  </div>
+		</div>
+	  </div>
+	</section>
+<!-- End Video Promo Section -->
