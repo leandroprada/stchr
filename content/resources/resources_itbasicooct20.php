516b5e55f0a5f9547cdf7ea3fc0023d577879ad6
leandro prada <leandroprada@gmail.com>
2020-12-11 10:38:07 -0300

    added youtube links for oct 2020

--- content/resources/resources_itbasicooct20.php
+++ content/resources/resources_itbasicooct20.php
@@ -16,10 +16,7 @@
 							<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 1 Intensivo 1 21-Oct-2020 <a  href="#submenubasoct1" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
 								<div class="collapse list-unstyled" id="submenubasoct1">
 									
-									<video width="100%" height="450px"  controls>
-									  <source src="content/resources/videos/itcursointensivoOct20-1.mp4" type="video/mp4">
-									Your browser does not support the video tag.
-									</video>
+									<iframe width="560" height="315" src="https://www.youtube.com/embed/AJltEyX_has" frameborder="0" allow="accelerometer;  encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 								</div>
 							</li>
 						</ul>	
@@ -29,10 +26,7 @@
 							<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 1 Intensivo 2 22-Oct-2020 <a  href="#submenubasoct2" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
 								<div class="collapse list-unstyled" id="submenubasoct2">
 									
-									<video width="100%" height="450px"  controls>
-									  <source src="content/resources/videos/itcursointensivoOct20-2.mp4" type="video/mp4">
-									Your browser does not support the video tag.
-									</video>
+									<iframe width="560" height="315" src="https://www.youtube.com/embed/M0YC1YdWmxQ" frameborder="0" allow="accelerometer;  encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 								</div>
 							</li>
 						</ul>	
@@ -42,10 +36,7 @@
 							<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 1 Intensivo 3 23-Oct-2020 <a  href="#submenubasoct3" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
 								<div class="collapse list-unstyled" id="submenubasoct3">
 									
-									<video width="100%" height="450px"  controls>
-									  <source src="content/resources/videos/itcursointensivoOct20-3.mp4" type="video/mp4">
-									Your browser does not support the video tag.
-									</video>
+									<iframe width="560" height="315" src="https://www.youtube.com/embed/z-8u8vVy-Yo" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 								</div>
 							</li>
 						</ul>	
