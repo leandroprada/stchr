a2e102db436d0a7cf720ffcf53cfc32675143ccb
leandro prada <leandroprada@gmail.com>
2020-12-11 08:57:18 -0300

    added vids for avanzado may 2020

--- content/resources/resources_itavanzadomay20.php
+++ content/resources/resources_itavanzadomay20.php
@@ -12,28 +12,18 @@
 				<ul>
 					<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 2 Clase 1  09-MAY-2020 <a  href="#submenuavmay1" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
 						<div class="collapse list-unstyled" id="submenuavmay1">
-							
-							<video width="100%" height="450px"  controls>
-							  <source src="content/resources/videos/itavanzadomay20-1.mp4" type="video/mp4">
-							Your browser does not support the video tag.
-							</video>
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/E5qhkaH_Bag" frameborder="0" allow="accelerometer;  encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 						</div>
 					</li>
 					<li style="text-decoration:none;padding-bottom:10px; "> <h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 2 Clase 2 16-MAY-2020 <a  href="#submenuavmay2" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver </a></h2>							
 						<div class="collapse list-unstyled" id="submenuavmay2">
-							<video width="75%" height="450px"  controls>
-							  <source src="content/resources/videos/itavanzadomay20-2.mp4" type="video/mp4">
-							Your browser does not support the video tag.
-							</video>
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/61NhCeKIPGk" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 						</div>
 					</li>
 					<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 2 Clase 3 24-MAY-2020 <a  href="#submenuavmay3" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver </a> </h2>
 						<div class="collapse list-unstyled" id="submenuavmay3">
 													
-							<video width="75%" height="450px"  controls>
-							  <source src="content/resources/videos/itavanzadomay20-3.mp4" type="video/mp4">
-							Your browser does not support the video tag.
-							</video>
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/kUHHFt8MIUM" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 						</div>
 					</li>
 				</ul>
