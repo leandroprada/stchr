3833ad79c19ebe2defd19514a43058cf656a0a5b
leandro prada <leandroprada@gmail.com>
2020-12-11 11:11:31 -0300

    added youtube links for all videos

--- content/resources/resources_generico.php
+++ content/resources/resources_generico.php
@@ -10,7 +10,7 @@
           <div class="col-md-12 col-sm-12">
               <div class="video-promo-content text-center">
 				<h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Entrevista de DuHire a nuestra directora, Romina C&oacute;rdoba</h2>
-                <iframe width="75%"	 height="450px" src="https://www.youtube.com/embed/TCOEEOvKXXk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                <iframe width="75%"	 height="450px" src="https://www.youtube.com/embed/TCOEEOvKXXk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe> 
                 
               </div>
           </div>
