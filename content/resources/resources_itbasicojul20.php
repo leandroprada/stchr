876b363283fa9c17a94c2ca24471c3a600352a19
leandro prada <leandroprada@gmail.com>
2020-12-11 09:33:31 -0300

    added youtube links for jul and jun 2020

--- content/resources/resources_itbasicojul20.php
+++ content/resources/resources_itbasicojul20.php
@@ -16,10 +16,7 @@
 							<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 1 Clase 1 18 JUL-2020 <a  href="#submenubasicojul1" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
 								<div class="collapse list-unstyled" id="submenubasicojul1">
 									
-									<video width="100%" height="450px"  controls>
-									  <source src="content/resources/videos/itbasicojul20.mp4" type="video/mp4">
-									Your browser does not support the video tag.
-									</video>
+									<iframe width="560" height="315" src="https://www.youtube.com/embed/cldYqED72CE" frameborder="0" allow="accelerometer;  encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 								</div>
 							</li>
 						</ul>	
@@ -30,10 +27,7 @@
 							<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 1 Clase 2 25 JUL-2020 <a  href="#submenubasicojul2" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
 								<div class="collapse list-unstyled" id="submenubasicojul2">
 									
-									<video width="100%" height="450px"  controls>
-									  <source src="content/resources/videos/itbasicojul20-2.mp4" type="video/mp4">
-									Your browser does not support the video tag.
-									</video>
+									<iframe width="560" height="315" src="https://www.youtube.com/embed/S4qMLl7yso0" frameborder="0" allow="accelerometer;  encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 								</div>
 							</li>
 						</ul>	
@@ -45,10 +39,7 @@
 							<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 1 Clase 3 AGO-2020 <a  href="#submenubasicojul3" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
 								<div class="collapse list-unstyled" id="submenubasicojul3">
 									
-									<video width="100%" height="450px"  controls>
-									  <source src="content/resources/videos/itbasicojul20-3.mp4" type="video/mp4">
-									Your browser does not support the video tag.
-									</video>
+									<iframe width="560" height="315" src="https://www.youtube.com/embed/9LwUbdZ1gVU" frameborder="0" allow="accelerometer;  encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 								</div>
 							</li>
 						</ul>	
--- content/resources/resources_itbasicojun20.php
+++ content/resources/resources_itbasicojun20.php
@@ -16,10 +16,8 @@
 							<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 1 Clase 1 JUN-2020 <a  href="#submenubasico1" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
 								<div class="collapse list-unstyled" id="submenubasico1">
 									
-									<video width="100%" height="450px"  controls>
-									  <source src="content/resources/videos/itbasicojun20.mp4" type="video/mp4">
-									Your browser does not support the video tag.
-									</video>
+									<iframe width="560" height="315" src="https://www.youtube.com/embed/sURUQ-8Odc8" frameborder="0" allow="accelerometer; 
+									encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 								</div>
 							</li>
 						</ul>	
@@ -29,10 +27,7 @@
 							<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 1 Clase 2 19 JUN-2020 <a  href="#submenubasico2" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
 								<div class="collapse list-unstyled" id="submenubasico2">
 									
-									<video width="100%" height="450px"  controls>
-									  <source src="content/resources/videos/itbasicojun20-2.mp4" type="video/mp4">
-									Your browser does not support the video tag.
-									</video>
+									<iframe width="560" height="315" src="https://www.youtube.com/embed/Zfb-SkusUbE" frameborder="0" allow="accelerometer;  encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 								</div>
 							</li>
 						</ul>	
@@ -42,10 +37,7 @@
 							<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 1 Clase 3 25 JUN-2020 <a  href="#submenubasico3" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
 								<div class="collapse list-unstyled" id="submenubasico3">
 									
-									<video width="100%" height="450px"  controls>
-									  <source src="content/resources/videos/itbasicojun20-3.mp4" type="video/mp4">
-									Your browser does not support the video tag.
-									</video>
+									<iframe width="560" height="315" src="https://www.youtube.com/embed/E_eusQrni88" frameborder="0" allow="accelerometer;  encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 								</div>
 							</li>
 						</ul>	
