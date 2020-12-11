26402ece477c531cad6f1f99c1c20d10f1518b2f
leandro prada <leandroprada@gmail.com>
2020-12-11 10:42:11 -0300

    added youtube links for av nov 2020

--- content/resources/resources_itavanzadonov20.php
+++ content/resources/resources_itavanzadonov20.php
@@ -13,27 +13,17 @@
 					<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 2 Clase 1 14-Nov-2020 <a  href="#submenuavnov1" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
 						<div class="collapse list-unstyled" id="submenuavnov1">
 							
-							<video width="100%" height="450px"  controls>
-							  <source src="content/resources/videos/itavanzadonov20-1.mp4" type="video/mp4">
-							Your browser does not support the video tag.
-							</video>
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/-M2dESAhx9M" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 						</div>
 					</li>
 					<li style="text-decoration:none;padding-bottom:10px; "> <h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 2 Clase 2 22-Nov-2020 <a  href="#submenuavnov2" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver </a></h2>							
 						<div class="collapse list-unstyled" id="submenuavnov2">
-							<video width="75%" height="450px"  controls>
-							  <source src="content/resources/videos/itavanzadonov20-2.mp4" type="video/mp4">
-							Your browser does not support the video tag.
-							</video>
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/i_WgHMm3h5Y" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 						</div>
 					</li>
 					<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 2 Clase 3 29-Nov-2020 <a  href="#submenuavnov3" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver </a> </h2>
 						<div class="collapse list-unstyled" id="submenuavnov3">
-													
-							<video width="75%" height="450px"  controls>
-							  <source src="content/resources/videos/itavanzadonov20-3.mp4" type="video/mp4">
-							Your browser does not support the video tag.
-							</video>
+							<iframe width="560" height="315" src="https://www.youtube.com/embed/bvpe35ldhL8" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 						</div>
 					</li>
 					<!-- <li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Curso Selecci&oacute;n IT M&oacute;dulo 2 Bonus Clase 4 12-SEP-2020 <a  href="#submenuavnov4" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver </a> </h2>
