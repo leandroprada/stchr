bebbc263ab60c26106a4c6b6b1198f27c3bd0e5b
leandro prada <leandroprada@gmail.com>
2020-12-16 15:53:51 -0300

    added rrss for rrhh 1

--- content/resources/resources_rrsspararrhhdic20.php
+++ content/resources/resources_rrsspararrhhdic20.php
@@ -14,7 +14,7 @@
 						<div class="collapse list-unstyled" id="submenurrssrrhhdic201">
 							
 							<iframe width="560" height="315" src="https://www.youtube.com/embed/EkfC0b8kqAQ" frameborder="0" allow="accelerometer;
-						</div>
+						</div> 
 					</li>
 					
 					<li style="text-decoration:none;padding-bottom:10px; "><h2 class="wow zoomIn" data-wow-duration="1000ms" data-wow-delay="100ms">Potenci&aacute; tus redes sociales - RRHH 2<a  href="#submenurrssrrhhdic202" data-toggle="collapse" aria-expanded="false" class="btn btn-info btn-lg"  style="color:white;">Ver</a></h2>							
